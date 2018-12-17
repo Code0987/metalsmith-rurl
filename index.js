51a86260588b23d4448c7761404b145c24dc86e3
Code0987 <durgapalneeraj@gmail.com>
2018-12-17 22:34:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,18 @@ var path = require('path');
 module.exports = plugin;
 
 function plugin() {
-	return function (files, metalsmith, done) {
-		Object.keys(files).forEach(function (file) {
-			setImmediate(done);
+    return function (files, metalsmith, done) {
+        Object.keys(files).forEach(function (file) {
+            setImmediate(done);
 
-			var paths = files[file].path.split(path.sep);
-			paths.pop();
-			
-			console.log(paths);
+            var paths = files[file].path.split(path.sep);
+            paths.pop();
             
-			files[file].rurl = paths.join("/");
-		});
-	};
+            console.log(paths);
+            
+            files[file].rurl = paths.join("/");
+        });
+    };
 }
+
 
